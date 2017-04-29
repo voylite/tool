5aeb8aecea57300925c8a0f751506ae478d92faa
voylite <unni@voylite.com>
2017-04-29 16:19:53 +0530

    centos rename fix

--- imageExtract.sh
+++ imageExtract.sh
@@ -2,7 +2,7 @@ cd "$1var/import/image"
 unzip images.zip -d .
 rm -f images.zip
 folder=$(find . -mindepth 1 -maxdepth 1 -type d)
-find $folder/ -name '* *' -type f | rename 's/ /-/g';
+find $folder/ -name '* *' -type f -exec rename ' ' '-' {} \;
 find $folder/ -name '*.jpg' -type f -exec mv {} . \;
 rm -rf $folder
 chmod -R 777 ../image
