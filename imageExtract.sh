da1bc6de6d3339a5a256c36bccb1d07efcb30979
voylite <unni@voylite.com>
2017-04-29 14:59:59 +0530

    image permission from script

--- imageExtract.sh
+++ imageExtract.sh
@@ -5,3 +5,4 @@ folder=$(find . -mindepth 1 -maxdepth 1 -type d)
 find $folder/ -name '* *' -type f | rename 's/ /-/g';
 find $folder/ -name '*.jpg' -type f -exec mv {} . \;
 rm -rf $folder
+chmod -R 777 ../image
