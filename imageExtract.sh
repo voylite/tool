328274bc9e5cc5e234f5fee91b3d42013c6142f2
voylite <unni@voylite.com>
2017-05-10 11:57:11 +0530

    image folder space issue

--- imageExtract.sh
+++ imageExtract.sh
@@ -1,6 +1,7 @@
 cd "$1var/import/image"
 unzip images.zip -d .
 rm -f images.zip
+mv * img
 folder=$(find . -mindepth 1 -maxdepth 1 -type d)
 find $folder/ -name '* *' -type f -exec rename ' ' '-' {} \;
 find $folder/ -name '*.jpg' -type f -exec mv {} . \;
